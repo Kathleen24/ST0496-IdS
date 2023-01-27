6f186174faa4e2e1cf5a387f8d555f27a6e93b81
Simone Cisca <simone.cisca@studenti.unicam.it>
2023-01-27 11:54:16 +0100

    [ADD] Add abbonamento entity and related.

---
+++ backend/src/main/java/it/unicam/ids/backend/controller/AbbonamentoController.java
@@ -0,0 +1,65 @@
+package it.unicam.ids.backend.controller;
+
+import it.unicam.ids.backend.entity.Abbonamento;
+import it.unicam.ids.backend.entity.PianoTariffario;
+import it.unicam.ids.backend.service.AbbonamentoService;
+import it.unicam.ids.backend.service.PianoTariffarioService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping
+public class AbbonamentoController {
+    private final AbbonamentoService abbonamentoService;
+    private final PianoTariffarioService pianoTariffarioService;
+
+    public AbbonamentoController(AbbonamentoService abbonamentoService, PianoTariffarioService pianoTariffarioService) {
+        this.abbonamentoService = abbonamentoService;
+        this.pianoTariffarioService = pianoTariffarioService;
+    }
+
+    @GetMapping("/all")
+    public List<Abbonamento> getAllAbbonamenti(){
+        return abbonamentoService.getAllAbbonamenti();
+    }
+
+    @GetMapping("/all")
+    public List<Abbonamento> getAllAbbonamenti(@RequestParam Boolean attivo){
+        return abbonamentoService.getAllAbbonamenti(attivo);
+    }
+
+    @GetMapping("/{id}")
+    public Abbonamento getAbbonamento(@PathVariable Integer id){
+        return abbonamentoService.getAbbonamento(id);
+    }
+
+    @PostMapping("/add")
+    public Abbonamento addAbbonamento(@RequestBody Abbonamento abbonamento){
+        return abbonamentoService.addAbbonamento(abbonamento);
+    }
+
+    @PostMapping("/add")
+    public Abbonamento addAbbonamento(@RequestParam PianoTariffario pianoTariffario,
+                                      @RequestParam String descrizione,
+                                      @RequestParam Boolean attivo){
+        return abbonamentoService.addAbbonamento(pianoTariffario, descrizione, attivo);
+    }
+
+    @PostMapping("/update")
+    public void updateAbbonamento(@RequestBody Abbonamento abbonamento){
+        abbonamentoService.addAbbonamento(abbonamento);
+    }
+
+    @PostMapping("/update")
+    public void updateAbbonamento(@RequestParam PianoTariffario pianoTariffario,
+                                  @RequestParam String descrizione,
+                                  @RequestParam Boolean attivo){
+        abbonamentoService.addAbbonamento(pianoTariffario, descrizione, attivo);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteAbbonamento(@PathVariable Integer id){
+        abbonamentoService.deleteAbbonamento(id);
+    }
+}
---
+++ backend/src/main/java/it/unicam/ids/backend/entity/Abbonamento.java
@@ -0,0 +1,146 @@
+package it.unicam.ids.backend.entity;
+
+import jakarta.persistence.*;
+
+import java.util.Date;
+import java.util.Objects;
+
+@Entity
+public class Abbonamento {
+    @Id
+    @GeneratedValue
+    private Integer id;
+
+    @ManyToOne
+    @JoinColumn(name = "pianoTariffario", referencedColumnName = "id")
+    private PianoTariffario pianoTariffario;
+    private String descrizione;
+    private Date dataIscrizione;
+    private Date dataUltimoPagamento;
+    private Boolean attivo;
+
+    //region Costruttori
+    public Abbonamento() {
+    }
+
+    public Abbonamento(PianoTariffario pianoTariffario, String descrizione, Boolean attivo) {
+        this.pianoTariffario = pianoTariffario;
+        this.descrizione = descrizione;
+        this.dataIscrizione = new Date();
+        this.dataUltimoPagamento = null;
+        this.attivo = attivo;
+    }
+
+    public Abbonamento(Integer id, PianoTariffario pianoTariffario, String descrizione, Boolean attivo) {
+        this.id = id;
+        this.pianoTariffario = pianoTariffario;
+        this.descrizione = descrizione;
+        this.dataIscrizione = new Date();
+        this.dataUltimoPagamento = null;
+        this.attivo = attivo;
+    }
+
+    public Abbonamento(Abbonamento abbonamento) {
+        this.id = getId();
+        this.pianoTariffario = getPianoTariffario();
+        this.descrizione = getDescrizione();
+        this.dataIscrizione = getDataIscrizione();
+        this.dataUltimoPagamento = getDataUltimoPagamento();
+        this.attivo = getAttivo();
+    }
+
+    //endregion
+
+    //region Getter e Setter
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public PianoTariffario getPianoTariffario() {
+        return pianoTariffario;
+    }
+
+    public void setPianoTariffario(PianoTariffario pianoTariffario) {
+        this.pianoTariffario = pianoTariffario;
+    }
+
+    public String getDescrizione() {
+        return descrizione;
+    }
+
+    public void setDescrizione(String descrizione) {
+        this.descrizione = descrizione;
+    }
+
+    public Date getDataIscrizione() {
+        return dataIscrizione;
+    }
+
+    public void setDataIscrizione(Date dataIscrizione) {
+        this.dataIscrizione = dataIscrizione;
+    }
+
+    public Date getDataUltimoPagamento() {
+        return dataUltimoPagamento;
+    }
+
+    public void setDataUltimoPagamento(Date dataUltimoPagamento) {
+        this.dataUltimoPagamento = dataUltimoPagamento;
+    }
+
+    public Boolean getAttivo() {
+        return attivo;
+    }
+
+    public void setAttivo(Boolean attivo) {
+        this.attivo = attivo;
+    }
+    //endregion
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Abbonamento that = (Abbonamento) o;
+
+        if (!Objects.equals(id, that.id)) return false;
+        if (!Objects.equals(pianoTariffario, that.pianoTariffario))
+            return false;
+        if (!Objects.equals(descrizione, that.descrizione)) return false;
+        if (!Objects.equals(dataIscrizione, that.dataIscrizione))
+            return false;
+        if (!Objects.equals(dataUltimoPagamento, that.dataUltimoPagamento))
+            return false;
+        return Objects.equals(attivo, that.attivo);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id != null ? id.hashCode() : 0;
+        result = 31 * result + (pianoTariffario != null ? pianoTariffario.hashCode() : 0);
+        result = 31 * result + (descrizione != null ? descrizione.hashCode() : 0);
+        result = 31 * result + (dataIscrizione != null ? dataIscrizione.hashCode() : 0);
+        result = 31 * result + (dataUltimoPagamento != null ? dataUltimoPagamento.hashCode() : 0);
+        result = 31 * result + (attivo != null ? attivo.hashCode() : 0);
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "Abbonamento:" +
+                " id=" + id +
+                ", pianoTariffario=" + pianoTariffario +
+                ", descrizione='" + descrizione + '\'' +
+                ", dataIscrizione=" + dataIscrizione +
+                ", dataUltimoPagamento=" + dataUltimoPagamento +
+                ", attivo=" + attivo +
+                '.';
+    }
+}
